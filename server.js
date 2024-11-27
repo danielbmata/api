a6ea5bdae0accf5aef1a9c99b298e4bb41598e06
Daniel <114535778+danielbmata@users.noreply.github.com>
2024-11-27 11:00:44 -0300

    cofigurar o post e get

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+/*
+    Criar API de usuarios:
+    - Criar um usuario
+    - Listar todos os usuarios
+    - Alterar um usuario
+    - Deletar um usuario
+*/
 import express from 'express';
 
+
 const app = express();
+app.use(express.json());
+
+const usuarios = [];
+
+app.post('/usuarios', (req, res) => {
+
+    usuarios.push(req.body) // salvando o usuario dentro da variavel
+    res.send('Ok, postt')
+});
+
 
 app.get('/usuarios', (req, res) => {
-    res.send('Ok, tudo certo!')
-})
 
-app.listen(3000)
+    res.json(usuarios); 
+});
+
+app.listen(3000);
