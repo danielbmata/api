e6b077e65f997fe00d8638df74165ef0906855e9
Daniel <114535778+danielbmata@users.noreply.github.com>
2024-11-28 10:31:46 -0300

    criar Rota de update de usuarios

--- server.js
+++ server.js
@@ -4,25 +4,57 @@
     - Listar todos os usuarios
     - Alterar um usuario
     - Deletar um usuario
+
+    d7mata
+    GcFqZxbLba8OkVpL
 */
 import express from 'express';
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 
 const app = express();
 app.use(express.json());
 
-const usuarios = [];
+// Rota de criar usuario
+app.post('/usuarios', async (req, res) => {
 
-app.post('/usuarios', (req, res) => {
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age,
+        }
+    });
 
-    usuarios.push(req.body) // salvando o usuario dentro da variavel
-    res.send('Ok, postt')
+    res.status(201).json(req.body)  // alem de criar, mostrar o que eu criei
+
 });
 
 
-app.get('/usuarios', (req, res) => {
+// Rota de listar usuarios
+app.get('/usuarios', async (req, res) => {
 
-    res.json(usuarios); 
+    const users = await prisma.user.findMany();
+    res.status(200).json(users); // respondendo com o status da requisição e tambem os usuarios em json
 });
+
+
+// Rota de update de usuarios
+app.put('/usuarios/:id', async (req, res) => {
+    const user = await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age,
+        }
+    });
+
+    res.status(200).json(user);
+});
+
 
 app.listen(3000);
